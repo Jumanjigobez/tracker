374380c77646ebac0cfc9ec95afe04233de521b8
Jumachaje <73429193+Jumanjigobez@users.noreply.github.com>
2024-09-04 19:25:44 +0300

    Update on the Api_url

--- script/script.js
+++ script/script.js
@@ -1,91 +1,132 @@
 //Function to get dom elements
 
-elem = (x) =>{
-    return document.querySelector(x)
-}
-
+elem = (x) => {
+  return document.querySelector(x);
+};
 
 var loading = elem("#loading_screen");
 
-setTimeout(()=>{
-    loading.className = "hide";
-},3000);
+setTimeout(() => {
+  loading.className = "hide";
+}, 3000);
 //Search functions
 var search = elem(".search"),
-    ip_address_part = elem(".ip_address"),
-    location_part = elem(".location"),
-    timezone_part = elem(".timezone"),
-    isp_part = elem(".isp");
+  ip_address_part = elem(".ip_address"),
+  location_part = elem(".location"),
+  timezone_part = elem(".timezone"),
+  isp_part = elem(".isp");
 
+const getUserIP = () => {
+  return fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => data.ip)
+    .catch((error) => {
+      console.error("Error fetching IP:", error);
+      return null;
+    });
+};
+//URL Fetch part for the location of current user
+var usersIp_url = "";
+fetch("https://api.ipify.org?format=json")
+  .then((res) => res.json())
+  .then((data) => (usersIp_url = data.ip))
+  .catch((error) => {
+    console.error("Error fetching IP:", error);
+  });
 
+var url = `https://ipapi.co/${usersIp_url}/json/`;
 
-//URL Fetch part for the location
-let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_B4PNbRU4DcIkpFZ3vnP6TNPrayJKL&ip`
 var map = "";
 var marker = "";
 var myIcon = L.icon({
-      iconUrl: './images/icon-location.svg',
-      iconSize: [40, 50],
-      iconAnchor: [22, 94]
+  iconUrl: "./images/icon-location.svg",
+  iconSize: [40, 50],
+  iconAnchor: [22, 94],
+});
 
-    });
-fetch(url).then((response) => response.json())
-.then((data) =>{
-
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
     ip_address_part.innerText = `${data.ip}`;
 
-    location_part.innerText = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
+    location_part.innerText = `${data.city}, ${data.country} ${data.postal}`;
 
-    timezone_part.innerText = `UTC ${data.location.timezone}`;
+    timezone_part.innerText = `UTC ${data.utc_offset}`;
 
-    isp_part.innerText = `${data.isp}`;
+    isp_part.innerText = `${data.org}`;
 
     //map_part
+    if (map) {
+      // Remove the existing map
+      map.off();
+      map.remove();
+    }
 
-    map = L.map('map_part').setView([`${data.location.lat}`, `${data.location.lng}`], 13);
+    map = L.map("map_part").setView(
+      [`${data.latitude}`, `${data.longitude}`],
+      13
+    );
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 90,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 90,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
-
-    
-
-    marker = L.marker([`${data.location.lat}`, `${data.location.lng}`], {icon: myIcon}).addTo(map);
-
-
-    console.log(data);
-
-
-});
+    marker = L.marker([`${data.latitude}`, `${data.longitude}`], {
+      icon: myIcon,
+    }).addTo(map);
+  })
+  .catch((err) => {
+    console.error("Error fetching IP:", err);
+  });
 
 //Start Search function
 
-const startSearch = () =>{
-    let searchTerm = search.value;
+const startSearch = () => {
+  let searchTerm = search.value;
 
-    if(searchTerm==""){
-        alert("Please Enter any Ip Address or Domain Name");
-    }else{
-        let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_B4PNbRU4DcIkpFZ3vnP6TNPrayJKL&ipAddress=${searchTerm}&domain=${searchTerm}`;
+  if (searchTerm == "") {
+    alert("Please Enter Valid Ip Address");
+  } else {
+    let url = `https://ipapi.co/${searchTerm}/json/`;
 
-        fetch(url).then((res)=> res.json())
-        .then((data)=> {
-            ip_address_part.innerText = `${data.ip}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        ip_address_part.innerText = `${data.ip}`;
 
-        location_part.innerText = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
+        location_part.innerText = `${data.city}, ${data.country} ${data.postal}`;
 
-        timezone_part.innerText = `UTC ${data.location.timezone}`;
+        timezone_part.innerText = `UTC ${data.utc_offset}`;
 
-        isp_part.innerText = `${data.isp}`;
+        isp_part.innerText = `${data.org}`;
 
         //map_part
+        if (map) {
+          // Remove the existing map
+          map.off();
+          map.remove();
+        }
 
-        map.panTo(L.latLng(`${data.location.lat}`, `${data.location.lng}`));
+        map = L.map("map_part").setView(
+          [`${data.latitude}`, `${data.longitude}`],
+          13
+        );
 
-        marker = L.marker([`${data.location.lat}`, `${data.location.lng}`], {icon: myIcon}).addTo(map);
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 90,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(map);
 
-        });
-    }
-}
+        marker = L.marker([`${data.latitude}`, `${data.longitude}`], {
+          icon: myIcon,
+        }).addTo(map);
+      })
+      .catch((err) => {
+        console.error("Error fetching IP:", err);
+      });
+  }
+};
